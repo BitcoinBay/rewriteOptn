17a8c4ac49467bc748001010dcd9e7bca8acb1f7
Pradeep Kumar <45354395+pradeepp88@users.noreply.github.com>
2021-04-02 08:54:50 -0400

    adding keystore

--- src/utils/bch-js-utils.ts
+++ src/utils/bch-js-utils.ts
@@ -4,9 +4,9 @@ const BCHN_MAINNET = 'https://bchn.fullstack.cash/v4/'
 const TESTNET3 = 'https://testnet3.fullstack.cash/v4/'
 
 const bchjs = new BCHJS({
-  //  restURL: "https://bchn.fullstack.cash/v3/"
-  //  apiToken:
-  //    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOGUzMGU2MDIyMWMxMDAxMmFkOTQwNyIsImVtYWlsIjoibGlnaHRzd2FybUBnbWFpbC5jb20iLCJhcGlMZXZlbCI6MCwicmF0ZUxpbWl0IjozLCJpYXQiOjE1OTgwNTc4NjUsImV4cCI6MTYwMDY0OTg2NX0.fy9g4MedNl4-NweF1NPzmGF0R1SExE0OjoWz0UA5N3o"
+    // restURL: "https://testnet3.fullstack.cash/v4/"
+    // apiToken:
+    //   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjVlOGUzMGU2MDIyMWMxMDAxMmFkOTQwNyIsImVtYWlsIjoibGlnaHRzd2FybUBnbWFpbC5jb20iLCJhcGlMZXZlbCI6MCwicmF0ZUxpbWl0IjozLCJpYXQiOjE1OTgwNTc4NjUsImV4cCI6MTYwMDY0OTg2NX0.fy9g4MedNl4-NweF1NPzmGF0R1SExE0OjoWz0UA5N3o"
 });
 
 export { bchjs };
--- src/utils/trial.js
+++ src/utils/trial.js
@@ -4,7 +4,7 @@ const bchjs = new BCHJS();
 
 async function main() {
   const utxos = await bchjs.Ninsight.utxo("bitcoincash:qqakphm6jqeteh902n59h2jct706n4srpuzp95a5qh");
-  console.log(utxos);
+  console.log("Transactions are: ",utxos);
   // const utxoInfo = await bchjs.SLP.Utils.tokenUtxoDetails(
   //   utxos.utxos, 
   //   // Delay 100mS between processing UTXOs, to prevent rate-limit errors.
